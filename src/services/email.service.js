a0acd83448e8ee8130056f849da8ac5ee7178ee4
Your Name <your.email@example.com>
2026-08-05 17:09:34 +0500

    update email servirs

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
   auth: {
-    type: 'OAuth2',
+    type: "OAuth2",
     user: process.env.EMAIL_USER,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
