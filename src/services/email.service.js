405036c54e3f4d946a626ca8ca8f82925440a93d
Your Name <your.email@example.com>
2026-08-05 17:14:57 +0500

    update email tranpoter

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -4,7 +4,11 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, 
+  secure: false,
+  family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     type: "OAuth2",
     user: process.env.EMAIL_USER,
